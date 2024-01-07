aa8c5e6fa4a4a4e61c4c470f9ba687530fa1aac7
Arvin Vahidkia <arvinvahidkia1@gmail.com>
2024-01-07 17:40:44 +0330

    init

---
+++ Models/AddEditProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace MyEshop.Models
+{
+    public class AddEditProductViewModel
+    {
+        public int Id { get; set; }
+        
+        [Display(Name = "نام محصول")]
+        public string Name { get; set; }
+
+        [Display(Name = "توضیحات محصول")]
+        public string Description { get; set; }
+
+        [Display(Name = "قیمت محصول")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "موجودی")]
+        public int QuantityInStock { get; set; }
+        public IFormFile Picture { get; set; }
+        public List<Category> Categories { get; set; }
+        
+
+    }
+}
